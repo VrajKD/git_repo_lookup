83deb7f58422a6685294c811212d87f17f6140ec
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2020-01-30 13:41:33 +0530

    Moment.js for updated_at dates

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './RepoCard.css';
+import Moment from 'react-moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons'
 // import { faStar } from '@fortawesome/free-regular-svg-icons'
 
 class RepoCard extends React.Component {
     render() {
-        const { name, language, fork, description } = this.props.repo;
+        const { name, language, fork, description, updated_at } = this.props.repo;
         return (
             <div className="repo-card">
                 <div className="repo-card-left">
@@ -22,7 +23,7 @@ class RepoCard extends React.Component {
                             <span className="repo-language"><span className="repo-language-circle"></span>{language}</span> : ""}
                         {fork ?
                             <span className="repo-members"><FontAwesomeIcon className="repo-members-icon" icon={faCodeBranch} />651</span> : ""}
-                        <span>Updated 8 days ago.</span>
+                        <span>Updated <Moment fromNow>{updated_at}</Moment></span>
                     </div>
                 </div>
                 <div className="repo-card-right">
